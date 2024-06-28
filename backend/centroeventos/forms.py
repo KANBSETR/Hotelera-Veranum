0d080720298b623f204a8f9f7a6c2de4d6dc6505
Benjamín Emiliano González Araya <b.gonzalez5@duocuc.cl>
2024-06-28 11:08:00 -0400

    Forms Mejora

--- backend/centroeventos/forms.py
+++ backend/centroeventos/forms.py
@@ -5,3 +5,15 @@ class EventosForm(forms.ModelForm):
     class Meta:
         model = Eventos
         fields = ['nombre', 'fechaInicio', 'fechaTermino', 'descripcion', 'precio', 'estado', 'idHotel', 'idFormaPago', 'idEmpleado', 'idUsuario']
+        widgets = {
+            'nombre': forms.TextInput(attrs={'class': 'form-control'}),
+            'fechaInicio': forms.DateTimeInput(attrs={'class': 'form-control',  'type': 'datetime-local'}),
+            'fechaTermino': forms.DateTimeInput(attrs={'class': 'form-control',  'type': 'datetime-local'}),
+            'descripcion': forms.Textarea(attrs={'class': 'form-control'}),
+            'precio': forms.NumberInput(attrs={'class': 'form-control'}),
+            'estado': forms.TextInput(attrs={'class': 'form-control'}),
+            'idHotel': forms.Select(attrs={'class': 'form-control'}),
+            'idFormaPago': forms.Select(attrs={'class': 'form-control'}),
+            'idEmpleado': forms.Select(attrs={'class': 'form-control'}),
+            'idUsuario': forms.Select(attrs={'class': 'form-control'}),
+        }
--- backend/habitaciones/forms.py
+++ backend/habitaciones/forms.py
@@ -1,5 +1,6 @@
 from django.forms import ModelForm
 from core.models import RegistroHabitacion
+from django import forms
 
 class HabitacionForm(ModelForm):
     class Meta:
@@ -7,6 +8,23 @@ class HabitacionForm(ModelForm):
         fields = ['numero_habitacion', 'cant_dormitorios', 'cant_banos','cant_camas','tamano_camas',
                   'cant_personas_disp','descripcion','precio','estado_habitacion','id_servicio',
                   'id_tipo_habitacion','id_empleado','id_hotel']
+        widgets = {
+            'numero_habitacion': forms.TextInput(attrs={'class': 'form-control'}),
+            'cant_dormitorios': forms.NumberInput(attrs={'class': 'form-control'}),
+            'cant_banos': forms.NumberInput(attrs={'class': 'form-control'}),
+            'cant_camas': forms.NumberInput(attrs={'class': 'form-control'}),
+            'tamano_camas': forms.TextInput(attrs={'class': 'form-control'}),
+            'cant_personas_disp': forms.NumberInput(attrs={'class': 'form-control'}),
+            'descripcion': forms.Textarea(attrs={'class': 'form-control'}),
+            'precio': forms.NumberInput(attrs={'class': 'form-control'}),
+            'estado_habitacion': forms.TextInput(attrs={'class': 'form-control'}),
+            'id_servicio': forms.Select(attrs={'class': 'form-control'}),
+            'id_tipo_habitacion': forms.Select(attrs={'class': 'form-control'}),
+            'id_empleado': forms.Select(attrs={'class': 'form-control'}),
+            'id_hotel': forms.Select(attrs={'class': 'form-control'}),
+            
+            
+        }
         labels = {
             'numero_habitacion': 'Número de Habitación',
             'cant_dormitorios': 'Cantidad de Dormitorios',
